e3b0b2cffcda825a800b80e30f65c47d49fa8be8
Piotr Tutak <p.tutak@outlook.com>
2018-05-09 12:17:38 +0200

    client server

---
+++ client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  9 12:12:08 2018
+
+@author: ptutak
+"""
+
+import socket
+
+HOST = '192.168.0.199' # Enter IP or Hostname of your server
+PORT = 12000 # Pick an open Port (1000+ recommended), must match the server port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST,PORT))
+
+#Lets loop awaiting for your input
+while True:
+    command = input('Enter your command: ')
+    s.send(command)
+    reply = s.recv(1024)
+    if reply == 'Terminate':
+        break
+    print(reply)
