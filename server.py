eed529b9bc25b532b91521276221efa123b449c0
Piotr Tutak <p.tutak@outlook.com>
2018-05-09 12:40:56 +0200

    client server

--- server.py
+++ server.py
@@ -13,10 +13,10 @@ try:
 except socket.error:
     print('Bind failed ')
 
-    s.listen(5)
-    print('Socket awaiting messages')
-    (conn, addr) = s.accept()
-    print('Connected')
+s.listen(5)
+print('Socket awaiting messages')
+(conn, addr) = s.accept()
+print('Connected')
 
 # awaiting for message
 while True:
