89c178188625ba304f6f9df4989b3f970572e46e
Piotr Tutak <p.tutak@outlook.com>
2018-05-14 13:18:32 +0200

    camera

--- serverCamera.py
+++ serverCamera.py
@@ -1,37 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import picamera
-import time
+
 
 HOST = '192.168.0.199'
-PORT_CAMERA = 13000
-cameraSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    cameraSocket.bind((HOST, PORT_CAMERA))
-except socket.error:
-    print('Bind failed ')
-else:
-    
-    recording=False
-    camera=picamera.PiCamera()
-    try:
-        camera.resolution=(640,480)
-        cameraSocket.listen(5)
-        print('Camera waiting for connection')
-        (connection, address) = cameraSocket.accept()
-        connectionFile=connection.makefile('wb')
-        print('Camera connected')
-        #camera.start_preview()
-        #time.sleep(2)
-        camera.start_recording(connectionFile,format='h264',quality=23)
-        recording=True
-        while recording:
-            pass
-    except BrokenPipeError as e:
-        print(e)
-        print('hello')
-        if recording:
-            camera.stop_recording()
-    finally:
-        connection.close()
--- serverComm.py
+++ serverComm.py
@@ -2,26 +2,73 @@
 
 import socket
 import sys
+import picamera
+import time
+import threading
 HOST = 'raspberry' # Server IP or Hostname
 PORT_COMM = 12000 # Pick an open Port (1000+ recommended), must match the client sport
 commSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    commSocket.bind((HOST, PORT_COMM))
-except socket.error:
-    print('Bind or connection failed')
-    sys.exit(-1)
-try:
-    commSocket.listen(5)
-    print('Command socket awaiting messages')
-    (connection, addr) = commSocket.accept()
-    print('Connected')
-    while True:
-        command = connection.recv(1024).decode()
-        if command == 'Hello':
-            connection.send('Hello there!'.encode())
-        elif command == 'quit':
-            break
-except Exception as e:
-    print(e)
-finally:
-    connection.close()
+PORT_CAMERA = 13000
+
+class CameraThread(threading.Thread):
+    def __init__(self, *args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.cameraSocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.cameraSocket.bind((HOST, PORT_CAMERA))
+        self.recording=False
+        self.camera=picamera.PiCamera()
+    def run(self):
+        try:
+            self.camera.resolution=(640,480)
+            self.cameraSocket.listen(5)
+            print('Camera waiting for connection')
+            (self.connection, address) = self.cameraSocket.accept()
+            connectionFile=self.connection.makefile('wb')
+            print('Camera connected')
+            #camera.start_preview()
+            self.camera.start_recording(connectionFile,format='h264',quality=23)
+            self.recording=True
+            while self.recording:
+                time.sleep(60)
+        except Exception as e:
+            print(e)
+            print('hello')
+            if self.recording:
+                self.camera.stop_recording()
+        finally:
+            self.connection.close()
+    def stopCamera(self):
+        if self.recording:
+            self.recording=False
+            self.camera.stop_recording()
+            self.connection.close()
+            print('Camera waiting for connection')
+            (self.connection,address) = self.cameraSocket.accept()
+
+
+class CommandThread(threading.Thread):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.commSocket.bind((HOST, PORT_COMM))
+    def run(self):
+        self.commSocket.listen(5)
+        print('Command socket awaiting messages')
+        (connection, addr) = commSocket.accept()
+        print('Connected')
+        try:
+            while True:
+                command = connection.recv(1024).decode()
+                if command == 'Hello':
+                    connection.send('Hello there!'.encode())
+                elif command == 'quit':
+                    break
+        except Exception as e:
+            print(e)
+        finally:
+            connection.close()
+
+if __name__=='__main__':
+    cameraT=CameraThread()
+    commandT=CommandThread()
+    cameraT.start()
+    commandT.start()
